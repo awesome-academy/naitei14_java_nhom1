d165ecc622ad521cfc0ddf1f125410387060f1cb
Dungsora04 <tuandung040204@gmail.com>
2025-11-19 18:15:58 +0700

    create spring boot pj and configs

---
+++ FoodAndDrinkProject/src/main/java/org/example/foodanddrinkproject/FoodAndDrinkProjectApplication.java
@@ -0,0 +1,13 @@
+package org.example.foodanddrinkproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodAndDrinkProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodAndDrinkProjectApplication.class, args);
+    }
+
+}
---
+++ FoodAndDrinkProject/src/test/java/org/example/foodanddrinkproject/FoodAndDrinkProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.foodanddrinkproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodAndDrinkProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
